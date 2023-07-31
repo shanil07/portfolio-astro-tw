36730662f689c024cef5651eca25a60731ce2a39
Shanil De Silva <shanil@endowus.com>
2023-07-31 08:32:28 +0530

    Add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      white: "ffffff",
+      transparent: "transparent",
+      primary: "#91F9E5",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
