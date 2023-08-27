b69e375f53ad0b8950ae1e78470756fd03edfc34
Shanil De Silva <shanil@limarktech.com>
2023-08-27 12:05:13 +0530

    Refactor to use animate property without state

--- src/components/BackgroundBlur/BackgroundBlur.tsx
+++ src/components/BackgroundBlur/BackgroundBlur.tsx
@@ -5,19 +5,19 @@ type Props = {
 };
 
 const BackgroundBlur = ({ className }: Props) => {
-  const [mousePosition, setMousePosition] = useState({
-    x: 0,
-    y: 0,
-  });
-
   const ref = useRef<HTMLDivElement>(null);
 
   const handleMouseMove = (e: MouseEvent) => {
+    const { clientX, clientY } = e;
+    const circle = document.getElementById("blur-circle");
     requestAnimationFrame(() =>
-      setMousePosition({
-        x: e.clientX,
-        y: e.clientY,
-      })
+      circle?.animate(
+        {
+          left: `${clientX}px`,
+          top: `${clientY}px`,
+        },
+        { duration: 3000, fill: "forwards" }
+      )
     );
   };
 
@@ -31,17 +31,14 @@ const BackgroundBlur = ({ className }: Props) => {
   return (
     <div className={`${className} bg-default`}>
       <div
+        id="blur-circle"
         className={`h-[200px] aspect-square fixed -translate-x-1/2 -translate-y-1/2 
-        transition-[left,top,translate-x,translate-y] delay-0 ease duration-[0.5s]`}
-        style={{
-          left: mousePosition.x,
-          top: mousePosition.y,
-        }}
+        transition-[left,top,translate-x,translate-y,background-image] delay-0 ease duration-[0.5s]`}
         ref={ref}
       >
         <div
           className={`bg-blueGradient w-full h-full rounded-full animate-customSpin
-          `}
+          transition-[left,top,translate-x,translate-y,background] delay-0 ease duration-[0.5s]`}
         />
       </div>
     </div>
