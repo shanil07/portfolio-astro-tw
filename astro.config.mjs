f1011d2b3d4e07a306db1073e99249e70c7e39eb
Shanil De Silva <shanil@limarktech.com>
2023-08-29 06:55:33 +0530

    Update version and add outdir

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://shanil07.github.io/",
-  base: "/portfolio-astro-tw",
+  outDir: "D:Coding Projectsshanil07.github.io",
   integrations: [tailwind(), react()],
 });
