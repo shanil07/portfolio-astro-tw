6ff2df66b4866cd4a46acb1b3afa8b7f89b87420
Shanil De Silva <shanil@limarktech.com>
2023-08-29 06:57:19 +0530

    Remove build path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://shanil07.github.io/",
-  outDir: "D:Coding Projectsshanil07.github.io",
   integrations: [tailwind(), react()],
 });
