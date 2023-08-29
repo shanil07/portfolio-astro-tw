e1c5a7e5976536733a0f2df7155f3b550049f15a
Shanil De Silva <shanil@limarktech.com>
2023-08-29 06:44:19 +0530

    Add base path to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://shanil07.github.io/",
+  base: "/portfolio-astro-tw",
   integrations: [tailwind(), react()],
 });
