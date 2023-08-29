3e7ee3112f66150b9215626c5c1c60c9de7257ba
Shanil De Silva <shanil@limarktech.com>
2023-08-29 06:32:01 +0530

    Remove base path from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://shanil07.github.io/",
-  base: "/portfolio-astro-tw",
   integrations: [tailwind(), react()],
 });
